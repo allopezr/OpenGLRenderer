ce2f89f08804f3f1d5672e60ebabd6fa768adebd
Alfonso López <allopezr@ujaen.es>
2023-01-13 09:40:57 +0100

    Include txt files and update clion project

--- python/clion_updater/update_source_files.py
+++ python/clion_updater/update_source_files.py
@@ -1,7 +1,7 @@
 import os
 
 vs_folder = '../../vs/Source/'
-target_folders = ['Geometry', 'Rendering', 'Patterns', 'PrecompiledHeaders', 'Libraries']
+target_folders = ['Geometry', 'Rendering', 'Patterns', 'PrecompiledHeaders', 'Libraries', 'Fonts']
 assets_folder = 'Assets/'
 
 clion_folder = '../../clion/'
@@ -11,8 +11,17 @@ release_folder = 'cmake-build-release-visual-studio-x64/Release/'
 
 # Copy folders from vs to clion
 for folder in target_folders:
-    os.system('cp -r ' + vs_folder + folder + ' ' + clion_folder)
+    # Get absolute path
+    vs_path = os.path.abspath(os.path.join(vs_folder, folder))
+    clion_path = os.path.abspath(os.path.join(clion_folder, folder))
+
+    os.system('xcopy /s /i /y ' + vs_path + ' ' + clion_path)
+
+# Copy main.cpp
+os.system('xcopy /i /y ' + os.path.abspath(os.path.join(vs_folder, 'main.cpp')) + ' ' +
+          os.path.abspath(os.path.join(clion_folder)))
 
 # Copy assets folder to clion debug and release
-os.system('cp -r ' + vs_folder + assets_folder + ' ' + clion_folder + debug_folder)
-os.system('cp -r ' + vs_folder + assets_folder + ' ' + clion_folder + release_folder)
+vs_path = os.path.abspath(os.path.join(vs_folder, assets_folder))
+os.system('xcopy /s /i /y ' + vs_path + ' ' + os.path.abspath(os.path.join(clion_folder, debug_folder)))
+os.system('xcopy /s /i /y ' + vs_path + ' ' + os.path.abspath(os.path.join(clion_folder, release_folder)))
